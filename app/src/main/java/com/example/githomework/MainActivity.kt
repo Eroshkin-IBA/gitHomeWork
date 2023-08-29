282a2c45ff781114eded2ef287d5cacacf6f36b1
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2023-08-29 13:24:24 +0300

    counter button was added

--- app/src/main/java/com/example/githomework/MainActivity.kt
+++ app/src/main/java/com/example/githomework/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.githomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var counter: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = counter.toString()
+        binding.button.setOnClickListener {
+            ++counter
+            binding.textView.text = counter.toString()
+        }
     }
 }
