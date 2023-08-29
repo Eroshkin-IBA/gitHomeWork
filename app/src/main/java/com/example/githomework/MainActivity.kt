36027d919fae797fd0e9cb4d9ce34015b93f17e5
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2023-08-29 13:53:47 +0300

    feature was added

--- app/src/main/java/com/example/githomework/MainActivity.kt
+++ app/src/main/java/com/example/githomework/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.githomework
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.githomework.databinding.ActivityMainBinding
@@ -12,9 +13,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textView.text = counter.toString()
+        val intent = Intent(this, MainActivity2::class.java)
+
+        var randomInt = (1..6).random()
         binding.button.setOnClickListener {
             ++counter
             binding.textView.text = counter.toString()
+            if (counter == randomInt) {
+                intent.putExtra(MainActivity2.SOCCER, randomInt)
+                startActivity(intent)
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/githomework/MainActivity2.kt
+++ app/src/main/java/com/example/githomework/MainActivity2.kt
@@ -2,10 +2,19 @@ package com.example.githomework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.githomework.databinding.ActivityMain2Binding
+import com.example.githomework.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
+    companion object {
+        const val SOCCER = "soccer"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView2.text = "you pressed ${intent?.extras?.getInt(SOCCER)}"
     }
 }
